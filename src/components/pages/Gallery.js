8f5a5a84b4aa168731d7303ad941c5c838ea370d
joemienko <mienko@uw.edu>
2021-08-15 00:07:33 -0700

    react-ig-feed addition

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -1,6 +1,24 @@
 import React from "react"; 
 import "../../App.css"; 
+import InstagramFeed  from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
+// load the value in the server
+const { REACT_APP_INSTA_TEST_TOKEN } = process.env;
+
 
 export default function Gallery() {
-    return <h1 className="gallery">GALLERY</h1>;
+    return (
+        <div className="cards">
+            <h1>Some h1 input here</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                    <InstagramFeed token={ REACT_APP_INSTA_TEST_TOKEN }  counter="6"/>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+
