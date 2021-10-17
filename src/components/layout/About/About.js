c9b70843003878e0ef7291c37397d57e48e1a1ab
Umbel <79326975+Um-bel@users.noreply.github.com>
2021-10-17 13:57:42 -0400

    About section of home

---
+++ src/components/layout/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import "./About.css"; 
+
+export default function About() {
+
+    return (
+        <div className="about">
+            <h1>Info About the Tattooist</h1>
+        </div>
+    )
+}
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,11 +2,13 @@ import "@src/App.css";
 import HeroSection from "@components/layout/ViewTemplate/HeroSection/HeroSection";
 import React from "react";
 import Cards from "@components/layout/Cards/Cards";
+import About from '@components/layout/About/About'
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <About />
       <Cards />
     </>
   );
