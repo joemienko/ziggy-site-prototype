438145cd7635dea27d322d58d7b8abfe8e1e40eb
Umbel <79326975+Um-bel@users.noreply.github.com>
2021-10-20 17:30:57 -0400

    Cry for help

--- src/components/layout/Gallery/Gallery.js
+++ src/components/layout/Gallery/Gallery.js
@@ -6,14 +6,17 @@ import React from 'react'
 function GalleryHtml() {
     return (
         <div className="gallery">
-            <h1>Look At the Premade Tattoos</h1>
+            <h1>Look At the Premade Tattoos.... ALSO
+                Help with css (more info @components/layout/Gallery/Gallery.js)
+            </h1>
+            
             <div className="images" dangerouslySetInnerHTML={{__html: imageGallery}}></div>
         </div>
         // this might be bad later on because of cross site scripting
 
 
-        // also if you're reading this could you add css for those elements please, im having troubbles doing it myself
-        //i dont really care what it looks like all i want from it is that each image has the same dimentions and are in rows of three
+        //  if you're reading this could you add css for those elements please, im having troubbles doing it myself
+        // i dont really care what it looks like all i want from it is that each image has the same dimentions and are in rows of three
 
     )
 }
