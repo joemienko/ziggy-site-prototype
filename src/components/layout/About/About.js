bb83fc6a4bb5c719bc285f789e47b169c3172122
Umbel <79326975+Um-bel@users.noreply.github.com>
2021-10-20 17:24:14 -0400

    About section added

--- src/components/layout/About/About.js
+++ src/components/layout/About/About.js
@@ -5,7 +5,14 @@ export default function About() {
 
     return (
         <div className="about">
-            <h1>Info About the Tattooist</h1>
+            <h1>A Message From the Tattooist</h1>
+            <div className="bio">
+                My name is Zig Mienko, and I’m a non-binary handpoke tattoo artist. 
+            I’ve been tattooing professionally since 2019. My favorite subject matters are abstracts, human figures, floral and fauna. Currently I prioritize flash designs and customs that appeal to my style/interests. Outside of tattooing I enjoy other visual arts such as drawing and painting. Commissions are fully welcomed as well! Check out my portfolio for examples of both my tattoos and other pieces. Feel free to explore my content further @hardeasy.handpokes on Instagram! If you have any questions you can either search through my FAQ, DM me on Instagram, or email me at ziggymienko@gmail.com
+            </div>
+            <div>
+                please add better css for this ^^^
+            </div>          
         </div>
     )
 }
